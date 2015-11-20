98179e17abd9fdc061c87448c60503848e04a9ca
fetorres <fetorres@groupon.com>
2015-11-19 23:18:30 -0300

    adding random API return

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var port = 3000;
 
 app.use('/', express.static(path.join(__dirname, '')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/videos', function(req, res) {
-	fs.readFile(path.join(__dirname, 'videos.js'), function(err, data) {
-		res.setHeader('Cache-Control', 'no-cache');
-		res.json(JSON.parse(data));
-	});
+	var videos = require('./videos.js');
+	var index = Math.floor((Math.random() * videos.length));
+	res.send(videos[index]);
 });
-
-app.listen(port, function() {
+app.listen(3000, function() {
 	console.log('Hey! Listen! (on http://localhost:3000)');
 });
