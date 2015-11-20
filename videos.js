b87acf5a95cd13579984db6a835d30a2322ae2b1
fetorres <fetorres@groupon.com>
2015-11-19 23:05:49 -0300

    api data

---
+++ videos.js
@@ -0,0 +1,17 @@
+module.exports = [{
+    "id": "zRIbf6JqkNc",
+    "song": "Don't Cry",
+    "artist": "Guns N' Roses"
+}, {
+    "id": "tgIqecROs5M",
+    "song": "Sail",
+    "artist": "AWOLNATION"
+},{
+    "id": "o1tj2zJ2Wvg",
+    "song": "Welcome To The Jungle",
+    "artist": "Guns N' Roses"
+},{
+    "id": "dQw4w9WgXcQ",
+    "song": "Never Gonna Give You Up",
+    "artist": "Rick Astley"
+}];
